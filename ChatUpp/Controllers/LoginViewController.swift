818a3daef53bd28b4fb6ead957ad1d43189baa8e
Andrei <pripandrei@gmail.com>
2023-06-28 00:11:56 +0300

    removed storyboard

--- ChatUpp/Controllers/LoginViewController.swift
+++ ChatUpp/Controllers/LoginViewController.swift
@@ -12,6 +12,7 @@ class LoginViewController: UIViewController {
   
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .green
         // Do any additional setup after loading the view.
     }
 
