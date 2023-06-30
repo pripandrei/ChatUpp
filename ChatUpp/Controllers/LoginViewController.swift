f7b196a466fc6c886f1ff70c3d2c11b8c8cdeb8e
Andrei <pripandrei@gmail.com>
2023-06-30 15:46:54 +0300

    TEMP VERSION: added textFields as custom view and loop through on creation

--- ChatUpp/Controllers/LoginViewController.swift
+++ ChatUpp/Controllers/LoginViewController.swift
@@ -9,59 +9,32 @@ import UIKit
 
 class LoginViewController: UIViewController, UITextFieldDelegate {
     
-    private let nextButton = UIButton()
+    private let signUp = UIButton()
     
-    lazy private var mailTextField: UITextField = {
-        let mailTextField = UITextField()
-        mailTextField.delegate = self
-        return mailTextField
-    }()
+    private let stackView = UIStackView()
     
-    lazy var passwordTextField: UITextField = {
-        let passwordTextfield = UITextField()
-        passwordTextfield.delegate = self
-        return passwordTextfield
-    }()
-    
-    private func setUpMailTextField() {
-        view.addSubview(mailTextField)
-        
-        var constraints = [NSLayoutConstraint]()
-        
-        mailTextField.text = "Enter mail here"
-        mailTextField.borderStyle = .line
-        mailTextField.translatesAutoresizingMaskIntoConstraints = false
-        
-        constraints.append(mailTextField.topAnchor.constraint(equalTo: view.topAnchor, constant: 330))
-        constraints.append(mailTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor))
-        constraints.append(mailTextField.widthAnchor.constraint(equalToConstant: 200))
-        constraints.append(mailTextField.heightAnchor.constraint(equalToConstant: 50))
-        NSLayoutConstraint.activate(constraints)
-    }
-    
-    private func setUpPasswordTextField()
-    {
-        view.addSubview(passwordTextField)
-        
-        passwordTextField.text = "Enter password here"
-        passwordTextField.borderStyle = .line
-        passwordTextField.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            passwordTextField.topAnchor.constraint(equalTo: mailTextField.topAnchor, constant: 80),
-            passwordTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            passwordTextField.widthAnchor.constraint(equalToConstant: 200),
-            passwordTextField.heightAnchor.constraint(equalToConstant: 50)
-        ])
-    }
+//    lazy private var mailTextField: UITextField = {
+//        let mailTextField = UITextField()
+//        mailTextField.delegate = self
+//        return mailTextField
+//    }()
+//
+//    lazy var passwordTextField: UITextField = {
+//        let passwordTextfield = UITextField()
+//        passwordTextfield.delegate = self
+//        return passwordTextfield
+//    }()
   
     override func viewDidLoad()
     {
         super.viewDidLoad()
         
         setUpNextButton()
-        setUpMailTextField()
-        setUpPasswordTextField()
+        configureStackView()
+        setStackViewConstraints()
+        
+//        setUpMailTextField()
+//        setUpPasswordTextField()
         
         view.backgroundColor = .white
         title = "Sign in"
@@ -69,27 +42,118 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         navigationController?.navigationBar.prefersLargeTitles = true
     }
     
-    private func setUpNextButton()
-    {
-        view.addSubview(nextButton)
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if let text = textField.text {
+            print(text)
+        }
+//        print(textField.text)
+    }
+    
+    private func configureStackView() {
+        view.addSubview(stackView)
         
-        nextButton.configuration = .filled()
-        nextButton.configuration?.title = "Sign up"
-        nextButton.configuration?.baseBackgroundColor = .systemPink
-        nextButton.translatesAutoresizingMaskIntoConstraints = false
-        nextButton.addTarget(self, action: #selector(segueToSignUpVC), for: .touchUpInside)
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.spacing = 20
+//        stackView.backgroundColor = .green
+        
+        addTextFieldToStackView()
+    }
+    
+    private func setStackViewConstraints() {
+        stackView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            nextButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100.0),
-            nextButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            nextButton.widthAnchor.constraint(equalToConstant: 200),
-            nextButton.heightAnchor.constraint(equalToConstant: 50)
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.bottomAnchor.constraint(equalTo: signUp.topAnchor, constant: -250),
+            stackView.widthAnchor.constraint(equalToConstant: 200),
+            stackView.heightAnchor.constraint(equalToConstant: 100)
         ])
     }
+    
+    private func addTextFieldToStackView() {
+        let numberOfTextFields = 2
+        
+        let textFieldTitles = ["Enter mail here", "Enter password here"]
+        
+        for number in 1...numberOfTextFields {
+            let textField = CustomTextField()
+            textField.delegate = self
+            textField.text = textFieldTitles[number - 1]
+            stackView.addArrangedSubview(textField)
+        }
+    }
+    
+    private func setUpNextButton()
+    {
+        view.addSubview(signUp)
+        
+        signUp.configuration = .filled()
+        signUp.configuration?.title = "Sign up"
+        signUp.configuration?.baseBackgroundColor = .systemPink
+        signUp.translatesAutoresizingMaskIntoConstraints = false
+        signUp.addTarget(self, action: #selector(segueToSignUpVC), for: .touchUpInside)
+        
+        NSLayoutConstraint.activate([
+            signUp.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100.0),
+            signUp.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            signUp.widthAnchor.constraint(equalToConstant: 200),
+            signUp.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+//    private func setUpMailTextField() {
+//        view.addSubview(mailTextField)
+//
+//        var constraints = [NSLayoutConstraint]()
+//
+//        mailTextField.text = "Enter mail here"
+//        mailTextField.borderStyle = .line
+//        mailTextField.translatesAutoresizingMaskIntoConstraints = false
+//
+//        constraints.append(mailTextField.topAnchor.constraint(equalTo: view.topAnchor, constant: 330))
+//        constraints.append(mailTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor))
+//        constraints.append(mailTextField.widthAnchor.constraint(equalToConstant: 200))
+//        constraints.append(mailTextField.heightAnchor.constraint(equalToConstant: 50))
+//        NSLayoutConstraint.activate(constraints)
+//    }
+//
+//    private func setUpPasswordTextField()
+//    {
+//        view.addSubview(passwordTextField)
+//
+//        passwordTextField.text = "Enter password here"
+//        passwordTextField.borderStyle = .line
+//        passwordTextField.translatesAutoresizingMaskIntoConstraints = false
+//
+//        NSLayoutConstraint.activate([
+//            passwordTextField.topAnchor.constraint(equalTo: mailTextField.topAnchor, constant: 80),
+//            passwordTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+//            passwordTextField.widthAnchor.constraint(equalToConstant: 200),
+//            passwordTextField.heightAnchor.constraint(equalToConstant: 50)
+//        ])
+//    }
     
     @objc func segueToSignUpVC() {
         let signUpVC = SignUpViewController()
         navigationController?.pushViewController(signUpVC, animated: true)
     }
+}
 
+class CustomTextField: UITextField {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpTextField()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setUpTextField()
+    }
+    
+    private func setUpTextField() {
+        borderStyle = .line
+        translatesAutoresizingMaskIntoConstraints = false
+    }
 }
