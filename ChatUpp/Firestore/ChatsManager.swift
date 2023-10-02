5fa70c29a52deda91cc962f5ff02424c66b07246
Andrei <pripandrei@gmail.com>
2023-10-02 18:36:15 +0300

    added Chat,Message model,ChatManager

--- ChatUpp/Firestore/ChatsManager.swift
+++ ChatUpp/Firestore/ChatsManager.swift
@@ -6,8 +6,59 @@
 //
 
 import Foundation
+import FirebaseFirestore
+import FirebaseFirestoreSwift
 
+struct Chat: Codable {
+    let id: String
+    let members: [String]
+    let lastMessage: String?
+    
+    let messages: [Message]?
+}
+
+struct Message: Codable {
+    let id: String
+    let message_body: String
+    let sender_id: String
+    let image_url: String?
+    let timestamp: Date
+}
 
 final class ChatsManager {
+    
+    let shared = ChatsManager()
+    
+    private init() {}
+    
+    let chatsCollection = Firestore.firestore().collection("chats")
+    
+    private func chatDocument(chatID: String) -> DocumentReference {
+        return chatsCollection.document(chatID)
+    }
+    
+    //MARK: - CREATE NEW DOC
+    
+    func createNewDocument(chat: Chat, complition: @escaping (Bool) -> Void) {
+        
+        chatDocument(chatID: chat.id).getDocument { [weak self] docSnapshot, error in
+            
+            guard error == nil else { print(error!.localizedDescription) ; return }
+            
+            guard let docSnapshot = docSnapshot, docSnapshot.exists else {
+                complition(true)
+                return
+            }
+            
+            try? self?.chatDocument(chatID: chat.id).setData(from: chat, merge: false) { error in
+                guard error == nil else {
+                    print(error!.localizedDescription)
+                    return
+                }
+                complition(true)
+            }
+            
+        }
+    }
     
 }
