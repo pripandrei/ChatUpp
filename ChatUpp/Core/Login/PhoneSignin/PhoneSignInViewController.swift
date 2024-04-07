2f5203a87432c6a906e3fdce0ce3bb5af9663534
Andrei <pripandrei@gmail.com>
2024-04-07 16:46:55 +0300

    PhoneSignInVC refactore; added protocol textViewShadowConfigurable to separate file

--- ChatUpp/Core/Login/PhoneSignin/PhoneSignInViewController.swift
+++ ChatUpp/Core/Login/PhoneSignin/PhoneSignInViewController.swift
@@ -8,74 +8,12 @@
 import UIKit
 import FlagPhoneNumber
 
-
-protocol TextViewShadowConfigurable {
-    func setupTopShadow()
-    func setupBottomShadow()
-}
-
-extension TextViewShadowConfigurable where Self: UITextField
-{
-    func setupTopShadow() {
-        self.borderStyle = .none
-        self.layer.cornerRadius = self.intrinsicContentSize.height/2
-        self.layer.borderWidth = 1.5
-        self.layer.borderColor = #colorLiteral(red: 0.822324276, green: 0.8223242164, blue: 0.8223242164, alpha: 1)
-        self.backgroundColor = #colorLiteral(red: 0.7896713614, green: 0.7896713614, blue: 0.7896713614, alpha: 1)
-        
-        let innerShadow = CALayer()
-        innerShadow.frame = self.bounds
-        
-        // Shadow path (1pt ring around bounds)
-        let radius = self.intrinsicContentSize.height/2
-        let path = UIBezierPath(roundedRect: innerShadow.bounds.insetBy(dx: -7, dy: -7), cornerRadius: radius)
-        let cutout = UIBezierPath(roundedRect: innerShadow.bounds, cornerRadius: radius).reversing()
-        
-        path.append(cutout)
-        
-        innerShadow.shadowPath = path.cgPath
-        innerShadow.masksToBounds = true
-        
-        // Shadow properties
-        innerShadow.shadowColor = #colorLiteral(red: 0.2635404468, green: 0.2457663417, blue: 0.2927972674, alpha: 1)
-        innerShadow.shadowOffset = CGSize(width: 3.5, height: 3.5)
-        innerShadow.shadowOpacity = 0.7
-        innerShadow.shadowRadius = 1.8
-        innerShadow.cornerRadius = self.intrinsicContentSize.height/2
-        self.layer.addSublayer(innerShadow)
-    }
-    
-    func setupBottomShadow() {
-        let shadowLayer = CALayer()
-        //
-        shadowLayer.frame = self.bounds
-        shadowLayer.shadowColor = #colorLiteral(red: 0.8560417295, green: 0.8963857889, blue: 0.8623355031, alpha: 1).cgColor
-        shadowLayer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        shadowLayer.shadowOpacity = 0.7
-        shadowLayer.shadowRadius = 1.3
-        shadowLayer.masksToBounds = true
-        shadowLayer.cornerRadius = self.intrinsicContentSize.height/2
-        //        shadowLayer.borderWidth = 1
-        
-        // Adjust the position of the shadow to the bottom right
-        let radius = self.intrinsicContentSize.height/2
-        let shadowPath = UIBezierPath(roundedRect: shadowLayer.bounds.offsetBy(dx: -3.5, dy: -3.5), cornerRadius: radius)
-        let cutout = UIBezierPath(roundedRect: shadowLayer.bounds, cornerRadius: radius).reversing()
-        
-        shadowPath.append(cutout)
-        shadowLayer.shadowPath = shadowPath.cgPath
-        //        layoutIfNeeded()
-        self.layer.addSublayer(shadowLayer)
-    }
-}
-
-
 class PhoneSignInViewController: UIViewController , UITextFieldDelegate {
     
     weak var coordinator: Coordinator!
     
     let phoneViewModel = PhoneSignInViewModel()
-    let phoneNumberTextField = CustomizedShadowTextField()
+    let customizedFPNTextField = CustomFPNTextField()
     let receiveMessageButton = UIButton()
     
     override func viewDidLoad() {
@@ -93,24 +31,24 @@ class PhoneSignInViewController: UIViewController , UITextFieldDelegate {
     }
     
     func setupPhoneTextField() {
-        view.addSubview(phoneNumberTextField)
+        view.addSubview(customizedFPNTextField)
         
-        phoneNumberTextField.delegate = self
+        customizedFPNTextField.delegate = self
 //        phoneNumberTextField.placeholder = "enter phone number"
-        phoneNumberTextField.borderStyle = .roundedRect
+        customizedFPNTextField.borderStyle = .roundedRect
         
-        phoneNumberTextField.translatesAutoresizingMaskIntoConstraints = false
+        customizedFPNTextField.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            phoneNumberTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            phoneNumberTextField.topAnchor.constraint(equalTo: view.topAnchor, constant: 250),
-            phoneNumberTextField.widthAnchor.constraint(equalToConstant: view.bounds.width * 0.7),
-            phoneNumberTextField.heightAnchor.constraint(equalToConstant: 45)
+            customizedFPNTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            customizedFPNTextField.topAnchor.constraint(equalTo: view.topAnchor, constant: 250),
+            customizedFPNTextField.widthAnchor.constraint(equalToConstant: view.bounds.width * 0.7),
+            customizedFPNTextField.heightAnchor.constraint(equalToConstant: 45)
         ])
         
-        phoneNumberTextField.layoutIfNeeded()
-        phoneNumberTextField.setupTopShadow()
-        phoneNumberTextField.setupBottomShadow()
+        customizedFPNTextField.layoutIfNeeded()
+        customizedFPNTextField.setupTopShadow()
+        customizedFPNTextField.setupBottomShadow()
 //        phoneNumberTextField.setupShadows()
     }
     
@@ -125,7 +63,7 @@ class PhoneSignInViewController: UIViewController , UITextFieldDelegate {
         
         NSLayoutConstraint.activate([
             receiveMessageButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            receiveMessageButton.topAnchor.constraint(equalTo: phoneNumberTextField.bottomAnchor, constant: 30),
+            receiveMessageButton.topAnchor.constraint(equalTo: customizedFPNTextField.bottomAnchor, constant: 30),
 //            receiveMessageButton.widthAnchor.constraint(equalToConstant: 200),
 //            receiveMessageButton.heightAnchor.constraint(equalToConstant: 40)
             
@@ -136,7 +74,7 @@ class PhoneSignInViewController: UIViewController , UITextFieldDelegate {
     }
 
     @objc func receiveMessageButtonWasTapped() {
-        guard let number = phoneNumberTextField.text, !number.isEmpty else {return}
+        guard let number = customizedFPNTextField.getFormattedPhoneNumber(format: .E164), !number.isEmpty else {return}
         
         Task {
             do {
@@ -151,84 +89,75 @@ class PhoneSignInViewController: UIViewController , UITextFieldDelegate {
    //MARK: - TEXTFIELD DELEGATE
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        phoneNumberTextField.resignFirstResponder()
+        customizedFPNTextField.resignFirstResponder()
         return true
     }
 }
 
-    //MARK: - CUSTOMIZED TEXTFIELD WITH SHADOWS
 
-class CustomizedShadowTextField: UITextField, TextViewShadowConfigurable {
+//MARK: - CUSTOMIZED FLAG PHONE NUMBER TEXTFIELD
+class CustomFPNTextField: FPNTextField, TextViewShadowConfigurable {
     
-//    convenience init() {
-//        self.init(frame: .zero)
-//        
-//    }
-//    override init(frame: CGRect) {
-//        super.init(frame: frame)
-//    }
-//    required init?(coder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-//    
+    private let separatorBetweenDialCodeAndTextPhone: UIView = UIView()
     
-//    func setupShadows() {
-//        setupTopShadow()
-//        setupBottomShadow()
-//    }
-//
-//    private func setupTopShadow() {
-//        self.borderStyle = .none
-//        self.layer.cornerRadius = self.intrinsicContentSize.height/2
-//        self.layer.borderWidth = 1.5
-//        self.layer.borderColor = #colorLiteral(red: 0.822324276, green: 0.8223242164, blue: 0.8223242164, alpha: 1)
-//        self.backgroundColor = #colorLiteral(red: 0.7896713614, green: 0.7896713614, blue: 0.7896713614, alpha: 1)
-//
-//        let innerShadow = CALayer()
-//        innerShadow.frame = self.bounds
-//
-//        // Shadow path (1pt ring around bounds)
-//        let radius = self.intrinsicContentSize.height/2
-//        let path = UIBezierPath(roundedRect: innerShadow.bounds.insetBy(dx: -7, dy: -7), cornerRadius: radius)
-//        let cutout = UIBezierPath(roundedRect: innerShadow.bounds, cornerRadius: radius).reversing()
-//
-//        path.append(cutout)
-//
-//        innerShadow.shadowPath = path.cgPath
-//        innerShadow.masksToBounds = true
-//
-//        // Shadow properties
-//        innerShadow.shadowColor = #colorLiteral(red: 0.2635404468, green: 0.2457663417, blue: 0.2927972674, alpha: 1)
-//        innerShadow.shadowOffset = CGSize(width: 3.5, height: 3.5)
-//        innerShadow.shadowOpacity = 0.7
-//        innerShadow.shadowRadius = 1.8
-//        innerShadow.cornerRadius = self.intrinsicContentSize.height/2
-//        self.layer.addSublayer(innerShadow)
-//    }
-//
-//    private func setupBottomShadow() {
-//        let shadowLayer = CALayer()
-//
-//        shadowLayer.frame = self.bounds
-//        shadowLayer.shadowColor = #colorLiteral(red: 0.8560417295, green: 0.8963857889, blue: 0.8623355031, alpha: 1).cgColor
-//        shadowLayer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-//        shadowLayer.shadowOpacity = 0.7
-//        shadowLayer.shadowRadius = 1.3
-//        shadowLayer.masksToBounds = true
-//        shadowLayer.cornerRadius = self.intrinsicContentSize.height/2
-//        //        shadowLayer.borderWidth = 1
-//
-//        // Adjust the position of the shadow to the bottom right
-//        let radius = self.intrinsicContentSize.height/2
-//        let shadowPath = UIBezierPath(roundedRect: shadowLayer.bounds.offsetBy(dx: -3.5, dy: -3.5), cornerRadius: radius)
-//        let cutout = UIBezierPath(roundedRect: shadowLayer.bounds, cornerRadius: radius).reversing()
-//
-//        shadowPath.append(cutout)
-//        shadowLayer.shadowPath = shadowPath.cgPath
-////        layoutIfNeeded()
-//        self.layer.addSublayer(shadowLayer)
-//    }
+    var dialCodeAndFlagButtonMainContainer: UIView? {
+        return flagButton.superview
+    }
+    
+    convenience init() {
+        self.init(frame: .zero)
+        
+        setupSeparator()
+        moveRightDialCodeAndFlagButtonMainContainer()
+    }
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - GESTURE
+    
+    func addGestureRecognizerToDialCode(_ tapGestureRecognizer: UITapGestureRecognizer) {
+        dialCodeAndFlagButtonMainContainer?.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    //MARK: - TEXTVIEW UI SETUP
+    
+    private func moveRightDialCodeAndFlagButtonMainContainer() {
+        dialCodeAndFlagButtonMainContainer?.subviews.forEach({ view in
+            view.transform = CGAffineTransform(translationX: 5, y: 0)
+        })
+    }
+    
+    func setupSeparator() {
+        dialCodeAndFlagButtonMainContainer?.addSubview(separatorBetweenDialCodeAndTextPhone)
+        separatorBetweenDialCodeAndTextPhone.backgroundColor = .black
+        
+        separatorBetweenDialCodeAndTextPhone.translatesAutoresizingMaskIntoConstraints = false
+        
+        separatorBetweenDialCodeAndTextPhone.widthAnchor.constraint(equalToConstant: 1).isActive = true
+        separatorBetweenDialCodeAndTextPhone.heightAnchor.constraint(equalToConstant: self.intrinsicContentSize.height).isActive = true
+        separatorBetweenDialCodeAndTextPhone.leadingAnchor.constraint(equalTo: dialCodeAndFlagButtonMainContainer!.trailingAnchor, constant: 8).isActive = true
+        separatorBetweenDialCodeAndTextPhone.centerYAnchor.constraint(equalTo: dialCodeAndFlagButtonMainContainer!.centerYAnchor).isActive = true
+        //        separatorBetweenDialCodeAndTextPhone.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        //        separatorBetweenDialCodeAndTextPhone.leadingAnchor.constraint(equalTo: dialCodeView!.trailingAnchor, constant: 7).isActive = true
+    }
 }
 
+
+//MARK: - TEXTFIELD TEXTRECT ADJUST
+extension CustomFPNTextField {
+    open override func textRect(forBounds bounds: CGRect) -> CGRect {
+        var rect = super.textRect(forBounds: bounds)
+        rect.origin.x += 20
+        rect.size.width -= 30
+        return rect
+    }
+    open override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return textRect(forBounds: bounds)
+    }
+}
 
 
