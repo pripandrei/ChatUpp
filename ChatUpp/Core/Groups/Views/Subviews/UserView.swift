ec7c64f0f4f2fc4de6a37bf251ed278118bc07f3
Andrei <pripandrei@gmail.com>
2025-05-23 22:26:00 +0300

    created UserViewViewModel and adjust UserView to display user image

--- ChatUpp/Core/Groups/Views/Subviews/UserView.swift
+++ ChatUpp/Core/Groups/Views/Subviews/UserView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 // MARK: - Users section
 struct UserView<Content: View>: View
 {
+    @StateObject private var viewModel: UserViewViewModel
+    
     private let userItem: User
     private let trailingItems: Content
     
@@ -18,14 +20,17 @@ struct UserView<Content: View>: View
     {
         self.userItem = userItem
         self.trailingItems = trailingItems()
+        
+        self._viewModel = StateObject(wrappedValue: UserViewViewModel(user: userItem))
     }
     
     var body: some View {
         HStack {
-            Circle()
-                .frame(width: 37, height: 37)
-                .padding(.trailing, 10)
-                .foregroundStyle(Color(ColorManager.actionButtonsTintColor))
+//            Circle()
+//                .frame(width: 37, height: 37)
+//                .padding(.trailing, 10)
+//                .foregroundStyle(Color(ColorManager.actionButtonsTintColor))
+            userImage()
             
             VStack(alignment: .leading) {
                 Text(userItem.name ?? "")
@@ -38,5 +43,60 @@ struct UserView<Content: View>: View
             }
             trailingItems
         }
+    }
+}
+
+extension UserView
+{
+    private func userImage() -> some View
+    {
+        let imageSize = 37.0
+        
+        if let imageData = viewModel.retrieveUserImageData(),
+           let image = UIImage(data: imageData)
+        {
+            return Image(uiImage: image)
+                .resizable()
+                .scaledToFill()
+                .frame(width: imageSize, height: imageSize)
+                .clipShape(Circle())
+            
+        } else {
+            return Image("default_profile_photo")
+                .resizable()
+                .scaledToFill()
+                .frame(width: imageSize, height: imageSize)
+//                .foregroundStyle(Color(ColorManager.actionButtonsTintColor))
+//                .background(Color(ColorManager.mainAppBackgroundColorGradientTop))
+                .clipShape(Circle())
+        }
+    }
+}
+
+
+final class UserViewViewModel: SwiftUI.ObservableObject
+{
+//    @Published var userImageData: Data?
+    private let user: User
+    
+    init(user: User)
+    {
+        self.user = user
+//        retrieveUserImageData()
+    }
+    
+    var imageURL: String?
+    {
+        if let imageURL = user.photoUrl {
+            return imageURL.replacingOccurrences(of: ".jpg", with: "_medium.jpg")
+        }
+        return nil
+    }
+    
+    func retrieveUserImageData() -> Data?
+    {
+        guard let url = imageURL else {return nil}
+//        self.userImageData = CacheManager.shared.retrieveImageData(from: url)
+        return CacheManager.shared.retrieveImageData(from: url)
     }
 }
